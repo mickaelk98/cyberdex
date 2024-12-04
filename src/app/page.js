93fb1773e2dcbe005565dc5cea84e5f6d24403d4
mickael keita <keitamickael@gmail.com>
2024-12-04 20:42:48 +0100

    Traduction des textes visibles en anglais

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { useState, useRef } from "react";
 import { gsap } from "gsap";
 import Image from "next/image";
@@ -14,14 +13,14 @@ export default function Home() {
   const [player2Choice, setPlayer2Choice] = useState("");
 
   // coin
-  const [result, setResult] = useState("pile");
+  const [result, setResult] = useState("tails");
   const [showResult, setShowResult] = useState(false);
   const [isFlipping, setIsFlipping] = useState(false);
   const coinRef = useRef(null);
 
   // Chemins des images
-  const pileImage = "/pile.png";
-  const faceImage = "/face.png";
+  const tailsImage = "/tails.png";
+  const headsImage = "/heads.png";
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -33,20 +32,20 @@ export default function Home() {
 
   function playerChoice(choice) {
     if (selectedPlayer === player1) {
-      if (choice === "pile") {
-        setPlayer1Choice("pile");
-        setPlayer2Choice("face");
+      if (choice === "tails") {
+        setPlayer1Choice("tails");
+        setPlayer2Choice("heads");
       } else {
-        setPlayer1Choice("face");
-        setPlayer2Choice("pile");
+        setPlayer1Choice("heads");
+        setPlayer2Choice("tails");
       }
     } else {
-      if (choice === "pile") {
-        setPlayer2Choice("pile");
-        setPlayer1Choice("face");
+      if (choice === "tails") {
+        setPlayer2Choice("tails");
+        setPlayer1Choice("heads");
       } else {
-        setPlayer2Choice("face");
-        setPlayer1Choice("pile");
+        setPlayer2Choice("heads");
+        setPlayer1Choice("tails");
       }
     }
   }
@@ -66,7 +65,7 @@ export default function Home() {
         ease: "power4.out",
         onComplete: () => {
           // Déterminer le résultat après le lancer
-          const outcome = Math.random() > 0.5 ? "pile" : "face";
+          const outcome = Math.random() > 0.5 ? "tails" : "heads";
           setResult(outcome);
           setIsFlipping(false);
         },
@@ -85,46 +84,44 @@ export default function Home() {
 
             <input
               className="p-1 text-slate-500 rounded-md"
-              placeholder="Joueur 1"
+              placeholder="Player 1"
               type="text"
               value={player1}
               onChange={(e) => setPlayer1(e.target.value)}
             />
             <input
               className="p-1 text-slate-500 rounded-md"
-              placeholder="Joueur 2"
+              placeholder="Player 2"
               type="text"
               value={player2}
               onChange={(e) => setPlayer2(e.target.value)}
             />
-            <button className="bg-blue-500 w-1/2 self-end text-white p-1 rounded-md ">Commencez</button>
+            <button className="bg-blue-500 w-1/2 self-end text-white p-1 rounded-md ">Start</button>
           </form>
 
-          {/* Informations affichées si les joueurs ont entré leur nom */}
           {showPlayer && (
             <ul className="flex gap-2 mt-8 items-center justify-between">
               <li className="w-1/2">
                 <div className="flex flex-col bg-white rounded-md">
-                  <h2 className="test-center bg-blue-500 p-1 rounded-tl-md rounded-tr-md text-center">Joueur 1</h2>
+                  <h2 className="test-center bg-blue-500 p-1 rounded-tl-md rounded-tr-md text-center">Player 1</h2>
                   <small className="text-center text-xl text-slate-500 py-2">{player1}</small>
                 </div>
               </li>
               <li className="w-1/2">
                 <div className="flex flex-col bg-white rounded-md">
-                  <h2 className="test-center bg-blue-500 p-1 rounded-tl-md rounded-tr-md text-center">Joueur 2</h2>
+                  <h2 className="test-center bg-blue-500 p-1 rounded-tl-md rounded-tr-md text-center">Player 2</h2>
                   <small className="text-center text-xl text-slate-500 py-2">{player2}</small>
                 </div>
               </li>
             </ul>
           )}
 
-          {/* Choix entre Pile ou Face */}
           {selectedPlayer && (
             <ul className="mt-4 flex flex-col items-center justify-center">
-              <p className="text-xl">{selectedPlayer} choisit entre pile ou face</p>
+              <p className="text-xl">{selectedPlayer} chooses between heads or tails</p>
               <div className="flex gap-2">
-                <Image className="cursor-pointer" src="/pile.png" alt="pile" width={150} height={150} onClick={() => playerChoice("pile")} />
-                <Image className="cursor-pointer" src="/face.png" alt="face" width={150} height={150} onClick={() => playerChoice("face")} />
+                <Image className="cursor-pointer" src="/tails.png" alt="heads" width={150} height={150} onClick={() => playerChoice("tails")} />
+                <Image className="cursor-pointer" src="/heads.png" alt="tails" width={150} height={150} onClick={() => playerChoice("heads")} />
               </div>
             </ul>
           )}
@@ -132,45 +129,43 @@ export default function Home() {
           {(player1Choice || player2Choice) && (
             <>
               {selectedPlayer === player1
-                ? <p className="text-center">{selectedPlayer} a choisit {player1Choice}, vous pouvez lancé la pièce</p>
-                : <p className="text-center">{selectedPlayer} a choisit {player2Choice}, vous pouvez lancé la pièce</p>}
+                ? <p className="text-center">{selectedPlayer} chose {player1Choice}, you can flip the coin</p>
+                : <p className="text-center">{selectedPlayer} chose {player2Choice}, you can flip the coin</p>}
             </>
           )}
 
-          {/* Resultat */}
           {showResult && isFlipping === false && <div className="flex items-center justify-evenly">
-            <h3>Resultat : </h3>
+            <h3>Result:</h3>
             <div className="flex gap-2 flex-col items-center justify-center">
-              <Image src={result === "pile" ? "/pile.png" : "/face.png"} alt="pile" width={150} height={150} />
-              <p>{result === "pile" ? "Pile" : "Face"}</p>
+              <Image src={result === "tails" ? "/tails.png" : "/heads.png"} alt="heads" width={150} height={150} />
+              <p>{result === "tails" ? "Heads" : "Tails"}</p>
             </div>
           </div>}
         </aside>
 
-        {/* CoinFlip occupe tout l'espace restant */}
         <div className="">
           <div className="flex flex-col items-center justify-center h-full">
-            <h1 className="text-3xl font-bold mb-6">Lancer de Pièce</h1>
+            <h1 className="text-3xl font-bold mb-6">Coin Toss</h1>
 
             <div
               ref={coinRef}
               className="w-32 h-32 bg- rounded-full flex items-center justify-center mb-6 relative"
             >
-              {result === "pile" && (
+              {result === "tails" && (
                 <Image
-                  src={pileImage}
+                  src={tailsImage}
                   width={400}
                   height={400}
-                  alt="Pile"
+                  alt="Heads"
                   className="w-full h-full object-cover"
                 />
               )}
-              {result === "face" && (
+              {result === "heads" && (
                 <Image
-                  src={faceImage}
+                  src={headsImage}
                   width={400}
                   height={400}
-                  alt="Face"
+                  alt="Tails"
                   className="w-full h-full object-cover"
                 />
               )}
@@ -182,7 +177,7 @@ export default function Home() {
               className={`bg-blue-500 text-white px-4 py-2 rounded ${isFlipping || player1Choice === "" || player2Choice === "" ? "opacity-50" : ""}`}
               disabled={isFlipping || player1Choice === "" || player2Choice === ""}
             >
-              {isFlipping ? "Lancer en cours..." : "Lancer la pièce !"}
+              {isFlipping ? "Flipping..." : "Flip the coin!"}
             </button>
           </div>
         </div>
