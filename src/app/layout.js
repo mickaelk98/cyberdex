3eff25a700ae27f9088b1d96aa548d977914f38f
mickael keita <keitamickael@gmail.com>
2024-12-04 21:03:31 +0100

    changement de la police

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,11 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+const pixelifySans = localFont({
+  src: "./fonts/PixelifySans-Medium.ttf",
+  variable: "--font-pixelify-sans",
+  weight: "100 900",
+})
 export const metadata = {
   title: "cyberdex",
   description: "Bienvenue sur CyberDeck, l'univers où la cybersécurité prend vie sous forme de cartes à collectionner et d'éléments interactifs! Découvrez un monde fascinant où hackers, experts en sécurité, outils technologiques, et menaces virtuelles s'affrontent dans des scénarios stratégiques captivants",
@@ -22,7 +27,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${pixelifySans.variable} antialiased`}
       >
         {children}
       </body>
