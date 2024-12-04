74e18ef64138293515e28824d95bb6ba28aeb8f9
mickael keita <keitamickael@gmail.com>
2024-12-04 20:31:57 +0100

    modification de la favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,8 @@ const geistMono = localFont({
 
 export const metadata = {
   title: "cyberdex",
-  description: "Bienvenue sur CyberDeck, l'univers où la cybersécurité prend vie sous forme de cartes à collectionner et d'éléments interactifs ! Découvrez un monde fascinant où hackers, experts en sécurité, outils technologiques, et menaces virtuelles s'affrontent dans des scénarios stratégiques captivants",
+  description: "Bienvenue sur CyberDeck, l'univers où la cybersécurité prend vie sous forme de cartes à collectionner et d'éléments interactifs! Découvrez un monde fascinant où hackers, experts en sécurité, outils technologiques, et menaces virtuelles s'affrontent dans des scénarios stratégiques captivants",
+  icons: "/cyberdex.png",
 };
 
 export default function RootLayout({ children }) {
